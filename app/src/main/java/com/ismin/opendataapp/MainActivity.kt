ed0e6771fc2000d2c5147019bc06b87940cdae50
Omar Khatib <khatibomar3@gmail.com>
2019-12-07 04:09:47 +0100

    Revert "Build files configurated for http requests, httprequest class added"
    This reverts commit 67d40d1c2b0e1c1596c11d06b5dd784029d7a948.

--- app/src/main/java/com/ismin/opendataapp/MainActivity.kt
+++ app/src/main/java/com/ismin/opendataapp/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.ismin.opendataapp
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
-val url="https://www.data.gouv.fr/fr/datasets/r/07a4a948-a51a-4aa4-ad20-ed4e1c83a832"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 }
