b9e9be3496efd8116c62ffecb587f3cde691efa2
Omar Khatib <khatibomar3@gmail.com>
2019-12-11 21:43:57 +0100

    Revert "Merge branch 'master' into Omar"
    This reverts commit 5508a7dcb64e1a49faea54ebcde2e32aa7b1d63c, reversing
    changes made to 2a7fec9d64eec82716d13edaae0bd0e11ead0726.

--- app/src/main/java/com/ismin/opendataapp/MainActivity.kt
+++ app/src/main/java/com/ismin/opendataapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.ismin.opendataapp
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import co.metalab.asyncawait.async
 import  com.ismin.opendataapp.webservice.RetrofitService
@@ -8,66 +7,17 @@ import  com.ismin.opendataapp.webservice.WomanWebService
 
 
 
-
-class MainActivity : AppCompatActivity(){
-
-    val manager = supportFragmentManager
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+            // Coroutine body
 
-//             val debugV = WomanWebService()
-//             debugV.main2()
+            val debugV = WomanWebService()
+            debugV.main2()
 
-        val list   = findViewById<Button>(R.id.list)
-        val map    = findViewById<Button>(R.id.MAPS)
-        val prefer = findViewById<Button>(R.id.preference)
-        val info   = findViewById<Button>(R.id.information)
-
-
-        list.setOnClickListener(){
-            ShowFragmentOne()
-        }
-        map.setOnClickListener(){
-            ShowFragmentMapTwo()
-        }
-        prefer.setOnClickListener(){
-            ShowFragmentThree()
-        }
-        info.setOnClickListener(){
-            ShowFragmentFour()
-        }
-    }
-
-    fun ShowFragmentOne(){
-        val transaction = manager.beginTransaction()
-        val frag = FragmentOne()
-        transaction.replace(R.id.a_main, frag) //replace the using one when you want to use another
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-    fun ShowFragmentMapTwo(){
-        val transaction = manager.beginTransaction()
-        val frag = FragmentMapTwo()
-        transaction.replace(R.id.a_main, frag) //replace the using one when you want to use another
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-    fun ShowFragmentThree(){
-        val transaction = manager.beginTransaction()
-        val frag = FragmentThree()
-        transaction.replace(R.id.a_main, frag) //replace the using one when you want to use another
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-    fun ShowFragmentFour(){
-        val transaction = manager.beginTransaction()
-        val frag = FragmentFour()
-        transaction.replace(R.id.a_main, frag) //replace the using one when you want to use another
-        transaction.addToBackStack(null)
-        transaction.commit()
     }
 
 
