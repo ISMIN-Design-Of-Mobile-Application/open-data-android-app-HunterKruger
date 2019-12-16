7a0c7e0a39ca7a63389073a4218e18db746de828
Omar Khatib <khatibomar3@gmail.com>
2019-12-16 21:05:39 +0100

    adding modification fromn Omar branch

--- app/src/main/java/com/ismin/opendataapp/webservice/RetrofitService.kt
+++ app/src/main/java/com/ismin/opendataapp/webservice/RetrofitService.kt
@@ -2,18 +2,15 @@ package com.ismin.opendataapp.webservice
 
 
 
-import android.database.Observable
 import com.ismin.opendataapp.Women
 
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 
  interface RetrofitService {
 
-    @GET(".")
-    //fun getFemme(@Path("recordid") recordid: String): Call<List<Femme>>
+    @GET(" ")
     fun findWomen():Call<List<Women>>
 }
 
--- app/src/main/java/com/ismin/opendataapp/webservice/WomanWebService.kt
+++ app/src/main/java/com/ismin/opendataapp/webservice/WomanWebService.kt
@@ -1,4 +1,5 @@
 package com.ismin.opendataapp.webservice
+import android.util.Log
 import androidx.lifecycle.LiveData
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,84 +16,68 @@ import retrofit2.Callback
 import retrofit2.Response
 
 
-internal class WomanWebService:WomenStorage {
+internal class WomanWebService {
+    private val women = ArrayList<Women>()
+    private lateinit var womenWebService: RetrofitService
 
-    private val liveData = MutableLiveData<ArrayList<Women>>()
 
-    var gson = GsonBuilder()
-        .setLenient()
-        .create()
+    // private val liveData = MutableLiveData<ArrayList<Women>>()
 
-     private val womenWebService = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(BASE_URL)
-        .build()
-         .create<RetrofitService>(RetrofitService::class.java)
 
-    init {
 
-        liveData.value = arrayListOf()
+    fun getBottles(): ArrayList<Women>{
+
+        return women
     }
 
+    fun gettingDatafromServer() {
 
-    override fun getAllWomen(): LiveData<ArrayList<Women>> {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+        womenWebService = retrofit.create(RetrofitService::class.java)
 
+        serverCommunication()
+    }
+
+    private fun serverCommunication() {
         womenWebService.findWomen()
-            .enqueue(object: Callback<List<Women>>{
-                override fun onResponse(call: Call<List<Women>>,
-                                        response: Response<List<Women>>
+            .enqueue(object : Callback<List<Women>> {
+                override fun onResponse(
+                    call: Call<List<Women>>,
+                    response: Response<List<Women>>
                 ) {
                     val serverWomen = response.body()
+                    println( serverWomen)
                     if (serverWomen != null) {
-                        liveData.value = ArrayList(serverWomen)
-                       println( "first women"+serverWomen.get(0))
-                        println("server is not null")
+                        women.clear()
+                        women.addAll(serverWomen)
+                        Log.d("problem", "good")
+
+                        println(women)
                     }
-                    else
-                        println("server is  null")
-
-
                 }
 
-                override fun onFailure(call: Call<List<Women>>,
-                                       t: Throwable) {
-
-                println("failure to  get women ")
+                override fun onFailure(
+                    call: Call<List<Women>>,
+                    t: Throwable
+                ) {
+                    Log.d("problem", "Unable to load cellar")
                 }
             })
-        return liveData
-            }
+
+
+    }
 
 
     companion object {
-        private const val BASE_URL = "https://opendata.paris.fr/api/records/1.0/search/?dataset=femmes-illustres-a-paris-portraits"
+        private const val BASE_URL =
+            "https://opendata.paris.fr/api/records/1.0/search/?dataset=femmes-illustres-a-paris-portraits&facet=name&facet=tab_name&facet=short_desc"
     }
 
-  //  val repository= womenWebService.create<RetrofitService>(RetrofitService::class.java)
 
 
 
-//    fun  main2() :LiveData<ArrayList<Women>>{
-//      //obalScope.launch {
-//            val requete=WomanWebService()
-//            val variables = requete.getAllWomen()
-//            return variables
-//        //this function tests the data getting from the json online
-//      //  val women = repository.findWomen()
-//        //println(women)
-//
-//        //val variables = women.execute().body()
-//       // println("after execute body")
-//         //   println(variables)
-//
-//       // variables?.forEach {
-//           // println(it)
-//
-//        //}
-
-
-
-    //    }
 }
-
 
